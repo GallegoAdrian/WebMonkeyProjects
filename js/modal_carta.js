fa1eae2b1056319449d6dd80496c76155b8ceba9
Carl Ruiz <carl.45.cr@gmail.com>
2016-11-22 17:09:53 +0100

    carta ok

--- js/modal_carta.js
+++ js/modal_carta.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal_carta = document.getElementById('myModal_carta');
+var modalCarta = document.getElementById('myModalCarta');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img;
@@ -17,16 +17,16 @@ for (i = 0; i < x.length; i++) {
 		//averiguamos nombre de imagen y ruta. luego la modificamos para que escoja otras imagenes más grandes
 		var str = this.src;
 		var res = str.replace("img/food/", "img/food/big/");
-		modal_carta.style.display = "block";
+		modalCarta.style.display = "block";
 		modalImg.src = res; /*this.src */
 		captionText.innerHTML = this.alt;
 	}
 
 	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close_carta")[0];
+	var span = document.getElementsByClassName("close-carta")[0];
 
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
-		modal_carta.style.display = "none";
+		modalCarta.style.display = "none";
 	}
 }
