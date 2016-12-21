b7ecc0d8b2658b0f20b96e7fcbcffe4bcc2984e9
Carl Ruiz <carl.45.cr@gmail.com>
2016-12-21 16:57:11 +0100

    menu refactor

--- js/menus.js
+++ js/menus.js
@@ -1,6 +1,7 @@
 /////////// Top Slider Menús//////////////////////
 
 $(document).ready( function() {
+  getSeason();
 
 // Cada seis segundos se ejecuta elswitchSliede() funtion.
 setInterval( "switchSlide()", 6000);
@@ -17,25 +18,29 @@ var altura = 0;
 ////////// Modal Menús//////////////////
 //Esta es la clase que hace de boton, y en el momento que haces
 // click se activa la funcion
+
 $(".bottom-title").on("click", ".myBtnMenu", function(){
+
 
   altura = $(window.document).scrollTop();
   $("body").css({"top": -altura});
   //Variable que contiene el numero del array para identifiacr 
   //el cuadrado con el que trabajar (esta funcion la hace el .index de jQuery)
-  var index = $(".myBtnMenu").index( this );
+  $index = $(".myBtnMenu").index( this );
   //Variable que contiene los elementos del cuadrado de su respectiva posicion(index)
   //(esta funcion la hace el .eq de jQuery)
-  $elem = $( ".info-content-menu" ).eq(index);
+  $elem = $( ".info-content-menu" ).eq($index);
+  
   //Variable en la que clono el contendio de elem para que al llevarmela
   //a la modal no se borre(la funcion que uso para eso es .clone de jQuery)
   $clone = $elem.clone( true );
+
   //Variable que contiene un span con una X para hacer el boton para cerrar la modal
   $span = $('<span class="close2">×</span>');
   //Variable en la que junto la X($span) + el contendio clonado($clone)
   //(lo hago con la funcion .add de jQuery)
   $more = $span.add($clone);
-  $botoDownload = $('<button class="boton">generate PDF</button>');
+  $botoDownload = $('<button id="boton">generate PDF</button>');
   
   $menu = $more.add($botoDownload);
   
@@ -79,14 +84,14 @@ var modalMenu = document.getElementsByClassName('myModalMenu')[0];
 //la funcion y compureba donde hemos hecho el clic
 window.onclick = function(event) {
   //Comprobamos si el clic esta dentro de nuestro modalMenu
-	if (event.target == modalMenu) {
+  if (event.target == modalMenu) {
     //Hacemos que el modalMenu desaparezca
-		$(modalMenu).fadeOut();
+    $(modalMenu).fadeOut();
      //Aqui volvemos a poner en nuestro css que el overflow este auto
      $("body").css({"position": "relative", "overflow-y": "auto","width": "100%"});
      $("body").css({"top": 0});
      $(window.document).scrollTop(altura);
-	}
+  }
 };
 //////////////Menús temporadas/////////////////////////
 function getSeason() {
@@ -97,8 +102,8 @@ function getSeason() {
         case '12':
         case '1':
         case '2':
-            season = '<div class="square-menus myBtnMenu">'+
-                        '<div class="info-content-menu">'+
+            season = '<div class="square-menus menu6" id="mySpecialButton">'+
+                        '<div id="menu-special">'+
                             '<h3>Menú Invierno</h3>'+
                             '<p>Un completo Menú de Mariscada en Palamós para disfrutarla en pareja o en grupo de amigos, comidas o cenas de empresa, celebraciones familiares...</p>'+
                               '<ul>'+
@@ -113,18 +118,18 @@ function getSeason() {
                                 '<li>Vino y bebidas</li>'+
                                 '<li>Cafés</li>'+
                               '</ul>'+
-                            '<h3>10,00€ (iva incl.)</h3>'+
-                            '<img class ="iconos" src="img/menus/gluten.png">'+
-                            '<img class ="iconos" src="img/menus/vegetariano.png">'+
-                            '<img class ="iconos" src="img/menus/vaca.png">'+
+                            '<span>10,00€</span>'+
+                            //'<img class ="iconos" src="img/menus/gluten.png">'+
+                            //'<img class ="iconos" src="img/menus/vegetariano.png">'+
+                            //'<img class ="iconos" src="img/menus/vaca.png">'+
                         '</div>'+               
                       '</div>';
         break;
         case '3':
         case '4':
         case '5':
-            season = '<div class="square-menus myBtnMenu">'+
-                        '<div class="info-content-menu">'+
+            season = '<div class="square-menus menu6" id="mySpecialButton">'+
+                        '<div id="menu-special">'+
                             '<h3>Menú Primavera</h3>'+
                             '<p>Un completo Menú de Mariscada en Palamós para disfrutarla en pareja o en grupo de amigos, comidas o cenas de empresa, celebraciones familiares...</p>'+
                               '<ul>'+
@@ -139,18 +144,18 @@ function getSeason() {
                                 '<li>Vino y bebidas</li>'+
                                 '<li>Cafés</li>'+
                               '</ul>'+
-                            '<h3>10,00€ (iva incl.)</h3>'+
-                            '<img class ="iconos" src="img/menus/gluten.png">'+
-                            '<img class ="iconos" src="img/menus/vegetariano.png">'+
-                            '<img class ="iconos" src="img/menus/vaca.png">'+
+                            '<span>10,00€</span>'+
+                            //'<img class ="iconos" src="img/menus/gluten.png">'+
+                            //'<img class ="iconos" src="img/menus/vegetariano.png">'+
+                            //'<img class ="iconos" src="img/menus/vaca.png">'+
                         '</div>'+               
                       '</div>';
         break;
         case '6':
         case '7':
         case '8':
-            season = '<div class="square-menus myBtnMenu">'+
-                        '<div class="info-content-menu">'+
+            season = '<div class="square-menus menu6" id="mySpecialButton">'+
+                        '<div id="menu-special">'+
                             '<h3>Menú Verano</h3>'+
                             '<p>Un completo Menú de Mariscada en Palamós para disfrutarla en pareja o en grupo de amigos, comidas o cenas de empresa, celebraciones familiares...</p>'+
                               '<ul>'+
@@ -165,18 +170,18 @@ function getSeason() {
                                 '<li>Vino y bebidas</li>'+
                                 '<li>Cafés</li>'+
                               '</ul>'+
-                            '<h3>10,00€ (iva incl.)</h3>'+
-                            '<img class ="iconos" src="img/menus/gluten.png">'+
-                            '<img class ="iconos" src="img/menus/vegetariano.png">'+
-                            '<img class ="iconos" src="img/menus/vaca.png">'+
+                            '<span>10,00€</span>'+
+                            //'<img class ="iconos" src="img/menus/gluten.png">'+
+                            //'<img class ="iconos" src="img/menus/vegetariano.png">'+
+                            //'<img class ="iconos" src="img/menus/vaca.png">'+
                         '</div>'+               
                       '</div>';
         break;
         case '9':
         case '10': 
         case '11':
-            season = '<div class="square-menus myBtnMenu">'+
-                        '<div class="info-content-menu menu">'+
+            season = '<div class="square-menus menu6" id="mySpecialButton">'+
+                        '<div id="menu-special">'+
                             '<h3>Menú Otoño</h3>'+
                             '<p>Un completo Menú de Mariscada en Palamós para disfrutarla en pareja o en grupo de amigos, comidas o cenas de empresa, celebraciones familiares...</p>'+
                               '<ul>'+
@@ -191,36 +196,118 @@ function getSeason() {
                                 '<li>Vino y bebidas</li>'+
                                 '<li>Cafés</li>'+
                               '</ul>'+
-                            '<h3>10,00€ (iva incl.)</h3>'+
+                            '<span>10,00€</span>'+
                             // '<img class ="iconos" src="img/menus/gluten.png">'+
                             // '<img class ="iconos" src="img/menus/vegetariano.png">'+
                             // '<img class ="iconos" src="img/menus/vaca.png">'+
-                        '</div>'+               
+                        '</div>'+
                       '</div>';
         break;
     }
     $(".bottom-title").append(season);
 }
-getSeason();
+
+
+$(".bottom-title").on("click", "#mySpecialButton", function(){
+
+console.log("hola");
+  altura = $(window.document).scrollTop();
+  $("body").css({"top": -altura});
+  //Variable que contiene el numero del array para identifiacr 
+  //el cuadrado con el que trabajar (esta funcion la hace el .index de jQuery)
+  $index = $(".myBtnMenu").index( this );
+  //Variable que contiene los elementos del cuadrado de su respectiva posicion(index)
+  //(esta funcion la hace el .eq de jQuery)
+  $elem = $( "#menu-special" );
+  
+  //Variable en la que clono el contendio de elem para que al llevarmela
+  //a la modal no se borre(la funcion que uso para eso es .clone de jQuery)
+  $clone = $elem.clone( true );
+
+  //Variable que contiene un span con una X para hacer el boton para cerrar la modal
+  $span = $('<span class="close2">×</span>');
+  //Variable en la que junto la X($span) + el contendio clonado($clone)
+  //(lo hago con la funcion .add de jQuery)
+  $more = $span.add($clone);
+  $botoDownload = $('<button id="botonSpecial">generate PDF</button>');
+  
+  $menu = $more.add($botoDownload);
+  
+  //$(".info-content-menu").append($botoDownload);
+  //La funcion .html añade los elementos + la X que tenemos en la 
+  //variable ($more) en la clase que pongamos, (esta funcion la hacemos con .html de jQuery)
+  $(".modal-content-menu").html($menu);
+
+  //Variable en la que junto la X($span) + el contendio clonado($clone)
+  //(lo hago con la funcion .add de jQuery)
+  //$moreBtn = $botoDownload.append($clone);
+
+  
+  //La funcion fadeIn hace que nos muestre la modal añadiendo un efecto de entrada
+  $(".myModalMenu").fadeIn();
+  //La funcion .css en este caso hace que añadamos al "body" el overflow: hidden,
+  //para hacer desaparecer el segundo scroll que sale cuando abrimos la modal.
+  //Este cambio solo se aplica cuando se abre la modal
+  $("body").css({"position": "fixed", "overflow-y": "scroll","width": "100%"});
+  //Aqui se acaba todas las funciones que hace el JS cuando lo abrimos.
+});
+//Hacemos una funcion que haga que cuando haces clic a "close2" se cierre la modal
+//Señalamos especificamente con la funcion .on que se aplique al hacer clic en "close2"
+$(".modal-content-menu").on("click", "span.close2", function(){
+    //La funcion fadeOut hace que ya no se muestre la modal con un efecto de salida
+    
+    $(".myModalMenu").fadeOut();
+    //Aqui volvemos a poner en nuestro css que el overflow este auto
+    $("body").css({"position": "relative", "overflow-y": "auto","width": "100%"});
+    $("body").css({"top": 0});
+
+    $(window.document).scrollTop(altura);
+    //Aqui acaba la funcion para cerrar la modal desde la "X"
+});
+
 
 ////////////  PDF ////////////////
 
-      $(".modal-content-menu").on("click", ".boton", function(){
+      $(".modal-content-menu").on("click", "#boton", function(){
           var doc = new jsPDF();
-              var source = $('.menu').html();
-              console.log(source);
+              var source = $(".modal-content-menu").html();
+              var res = source.replace('<span class="close2">×</span>', "");
+              var res2 = res.replace('<button id="botonSpecial">generate PDF</button>',"");
+              var res3 = res2.replace('€'," Euros ");
+
               var specialElementHandlers = {
                   '#bypassme': function (element, renderer) {
                       return true;
                   }
               };
-              doc.fromHTML(source, 0.5, 0.5, {
+              doc.fromHTML(res3, 0.5, 0.5, {
                   'width': 75,'elementHandlers': specialElementHandlers
               });
               console.log(doc);
 
               doc.save("menu.pdf");
       });
+
+      $(".modal-content-menu").on("click", "#botonSpecial", function(){
+          var doc = new jsPDF();
+              var source = $(".modal-content-menu").html();
+              var res = source.replace('<span class="close2">×</span>', "");
+              var res2 = res.replace('<button id="botonSpecial">generate PDF</button>',"");
+              var res3 = res2.replace('€'," Euros ");
+
+              var specialElementHandlers = {
+                  '#bypassme': function (element, renderer) {
+                      return true;
+                  }
+              };
+              doc.fromHTML(res3, 0.5, 0.5, {
+                  'width': 75,'elementHandlers': specialElementHandlers
+              });
+              console.log(doc);
+
+              doc.save("menu.pdf");
+      });
+
 
 // var pdf = new jsPDF('p', 'pt', 'letter');
 //  pdf.addHTML($('#content')[0], function () {
@@ -269,4 +356,3 @@ getSeason();
 //   }
 // };
 // doc.output("dataurlnewwindow");
-
