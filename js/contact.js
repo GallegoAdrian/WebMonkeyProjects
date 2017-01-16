a2d717b99f7e52a7dc3f522110cbb89521b5e700
Francesc Edo Trias <Francesc Edo Trias>
2017-01-16 19:36:45 +0100

    images recovery

--- js/contact.js
+++ js/contact.js
@@ -2,6 +2,7 @@ $( "#form" ).submit(function( event ) {
   //event.preventDefault();
   if ($('#contact-name').val().trim() == "" || $('#contact-email').val().trim() == "" || $('#contact-message').val().trim() == "") {
   	alert('empty');
+  	
   }
   else{
   	alert('Enviado');
