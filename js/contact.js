cee9dd4f0d1dc148f95f5c66be5c22545819c9f5
Francesc Edo Trias <Francesc Edo Trias>
2017-01-16 18:19:50 +0100

    error message contact

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+$( "#form" ).submit(function( event ) {
+  //event.preventDefault();
+  if ($('#contact-name').val().trim() == "" || $('#contact-email').val().trim() == "" || $('#contact-message').val().trim() == "") {
+  	alert('empty');
+  }
+  else{
+  	alert('Enviado');
+  }
+});
