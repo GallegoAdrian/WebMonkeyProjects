5c7fe246c72fd231ebc0e4a7db201c1459989744
Francesc Edo Trias <Francesc Edo Trias>
2017-01-17 17:35:08 +0100

    Image Alts done

--- js/header.js
+++ js/header.js
@@ -59,7 +59,7 @@ function getMenu(idioma) {
         var menu = 
         '<header class="nbg">'+
               '<div id="logo">'+
-                '<img src="img/header/Logo-degradado.png">'+
+                '<img alt="Logo La Bona Mar" src="img/header/Logo-degradado.png">'+
                 '<div id="info">'+
                   '<p>Telf. 972 310 000</p>'+
                   '<p>C/ Creu 22, 17230, Palamós-Girona</p>'+
@@ -72,13 +72,13 @@ function getMenu(idioma) {
                   '<li><a href="carta.html'+lang+'">'+short.chart+'</a></li>'+
                   '<li><a href="conocenos.html'+lang+'">'+short.know_us+'</a></li>'+
                   '<li><a href="contacto.html'+lang+'">'+short.contact+'</a></li>'+
-                  '<img id="myBtn" class="sun" src="img/header/sun-white.png" onClick="openModal();">'+
+                  '<img alt="Botón Tiempo" id="myBtn" class="sun" src="img/header/sun-white.png" onClick="openModal();">'+
                 '</ul>'+
               '</nav>'+
-          '</header>'+
+          '</header>'+ 
            '<header class="bg" hidden="true">'+
               '<div id="logo">'+
-                '<img src="img/header/Logo-degradado2.png">'+
+                '<img alt="Logo La Bona Mar" src="img/header/Logo-degradado2.png">'+
                 '<div id="info">'+
                   '<p>Telf. 972 310 000</p>'+
                   '<p>C/ Creu 22, 17230, Palamós-Girona</p>'+
@@ -91,7 +91,7 @@ function getMenu(idioma) {
                   '<li><a href="carta.html'+lang+'">'+short.chart+'</a></li>'+
                   '<li><a href="conocenos.html'+lang+'">'+short.know_us+'</a></li>'+
                   '<li><a href="contacto.html'+lang+'">'+short.contact+'</a></li>'+
-                  '<img id="myBtn2" class="sun" src="img/header/sun-black.png" onClick="openModal();">'+
+                  '<img alt="Logo Tiempo" id="myBtn2" class="sun" src="img/header/sun-black.png" onClick="openModal();">'+
                 '</ul>'+
               '</nav>'+
           '</header>';
